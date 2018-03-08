bb66267e2585039159243aaf46375bd1dce6bbef
Giorgio Giora <giorgio.giora@ocio.usda.gov>
2018-03-08 10:15:38 -0700

    user2a

--- Script_02/Action.c
+++ Script_02/Action.c
@@ -4,5 +4,13 @@
 
 Action()
 {
+	truclient_step("1", "Navigate to 'http://www.google.com/'", "snapshot=Action_1.inf");
+	truclient_step("3", "search (1)", "snapshot=Action_3.inf");
+	{
+		truclient_step("3.1", "Click on Search combobox", "snapshot=Action_3.1.inf");
+		truclient_step("3.3", "Type test123 in Search combobox", "snapshot=Action_3.3.inf");
+		truclient_step("3.4", "Press key Enter on Search combobox", "snapshot=Action_3.4.inf");
+	}
+
 	return 0;
 }
